6ac97a67683ac66f2d05e2157a88a8b410ed333e
Anil9337 <159010716+Anil9337@users.noreply.github.com>
2024-07-08 16:24:04 +0530

    Update rds.sh

--- rds.sh
+++ rds.sh
@@ -1,15 +1,15 @@
 
 resource "aws_instance" "instance1" {
-  ami                         = "ami-0195204d5dce06d99"
+  ami                         = "ami-066f98455b59ca1ee"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "anil"
-  vpc_security_group_ids     = ["sg-0640ac226b79673e7"]
-  subnet_id                   = "subnet-001dafb658ac7d5b2"
+  key_name                    = "kkkkk"
+  vpc_security_group_ids     = ["sg-0904496989ecd2a17"]
+  subnet_id                   = "subnet-09a39101a33901123"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
 
   tags = {
-    Name = "klin"
+    Name = "king"
   }
 }
