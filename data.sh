6bee500a7f2b6eaac08cc877e2d3e983d1533288
Anil9337 <159010716+Anil9337@users.noreply.github.com>
2024-07-08 16:00:22 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+yum update -y
+yum install -y git httpd docker
+systemctl enable httpd
+systemctl start httpd
+systemctl enable docker
+systemctl start docker
+usermod -a -G docker ec2-user
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+cd/tmp/
+cat <<EOF > docker-compose.yml
+services:
+  db:
+    image: mysql:8.0.15
+    container_name: wordpress_db
+    volumes:
+      - db_data:/var/lib/mysql
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: wordpress
+      MYSQL_DATABASE: anil
+      MYSQL_USER: virat
+      MYSQL_PASSWORD: anil1818
+
+  wordpress:
+    image: wordpress:latest
+    container_name: wordpress_app
+    depends_on:
+      - db
+    ports:
+      - "9999:80"
+    restart: always
+    environment:
+      WORDPRESS_DB_HOST: db:3306
+      WORDPRESS_DB_USER: virat
+      WORDPRESS_DB_PASSWORD: anil1818
+      WORDPRESS_DB_NAME: anil
+    volumes:
+      - wordpress_data:/var/www/html
+
+volumes:
+  db_data:
+  wordpress_data:
+EOF
+cd/tmp/
+sudo docker-compose -p my_project up
