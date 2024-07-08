50d4c12416132ec2f3fac4bd980149bc406dbf28
Anil9337 <159010716+Anil9337@users.noreply.github.com>
2024-07-08 16:03:40 +0530

    Create rds.sh

---
+++ rds.sh
@@ -0,0 +1,15 @@
+
+resource "aws_instance" "instance1" {
+  ami                         = "ami-0195204d5dce06d99"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "anil"
+  vpc_security_group_ids     = ["sg-0640ac226b79673e7"]
+  subnet_id                   = "subnet-001dafb658ac7d5b2"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+
+  tags = {
+    Name = "klin"
+  }
+}
